79236a599274a45481f33fe07223a1d2e87fc412
Erroler <fabio_the_boss@hotmail.com>
2021-04-28 15:00:40 +0100

    Made landing page more responsive.

--- frontend/src/components/landing/HeroText.js
+++ frontend/src/components/landing/HeroText.js
@@ -4,20 +4,20 @@ import { Link } from 'react-router-dom';
 const HeroText = () => {
     return (
         <div className="col-xl-7 col-12 w-100 m-0">
-            <h1 className="my-3 text-md-left" style={{ fontSize: "3.6rem", textShadow: '1px 1px 0 #cccccc70, 2px 2px 0 #ccc, 3px 3px 0 #4444442e, 2px 2px 0 #44444452, 2px 2px 0 #4444441f' }}>
+            <h1 className="my-3 text-md-left" id="landing-title">
                 <span className="blue-secondary">Des</span>
                 <span className="text-primary">informa</span>
                 <span className="blue-secondary">.me</span>
             </h1>
             <p className="lead text-justify text-lg-left">
                 Todos os dias milhares de notícias falsas espalham-se nas redes sociais como um vírus. Achas que as consegues identificar? Mete à prova o teu conhecimento num jogo divertido!                    </p>
-            <div className="d-inline flex-column flex-md-row mt-md-0 mt-5">
-                <Link to={'/jogo'} className="btn btn-primary col-md-5 col-lg mr-3 my-3 py-3 py-md-2 w-50">
+            <div className="d-flex flex-column flex-md-row flex-md-row mt-md-0 mt-3">
+                <Link to={'/jogo'} className="btn btn-primary col-md-5 col-lg mr-3 mt-3 mb-0 mb-md-3 py-3 py-md-2">
                     Novo Jogo
                 </Link>
-                {/* <Link to={'/jogo'} className="btn btn-secondary col-md-5 col-lg my-3 py-3 py-md-2 w-100">
+                <Link to={'/como-jogar'} className="btn btn-secondary col-md-5 col-lg my-3 py-3 py-md-2">
                     Como Jogar
-                </Link> */}
+                </Link>
             </div>
         </div>
     )
