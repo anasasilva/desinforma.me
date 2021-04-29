fbdddcfa0e7d11e3af07761f34ece306ce507c99
RMoura98 <ricardo.sanfinsmoura@gmail.com>
2021-04-29 17:43:52 +0100

    Fix news with no image problem
    .

--- frontend/src/components/CardContent.js
+++ frontend/src/components/CardContent.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 
 function CardContent(props) {
 
   const { news } = props;
+  const [showDefaultImage, setShowDefaultImage] = useState(false)
+  
+  const imageFirstFailHandler = (e) => {
+    e.target.src = e.target.src.replace(/(^https?:\/\/)?(www.)?arquivo\.pt\/noFrame\/replay\/\d+\//g, "")
+    e.target.onError = imageSecondFailHandler;
+  }
+
+  const imageSecondFailHandler = (e) => {
+    e.target.onError = null;
+    setShowDefaultImage(true)
+  }
+
 
   if (news.isFake) {
     return (
       <div className='tinder-card p-2'>
-        <div className="img-div">
-          <img src={news.fakeDetails.fakeImageUrl} alt={news.fakeDetails.fakeTitle} />
+        <div className="img-div h-100">
+          <img src={news.fakeDetails.fakeImageUrl} alt={news.fakeDetails.fakeTitle} className={(showDefaultImage ? "d-none" : "")} onError={imageFirstFailHandler}/>
+          <div className={"placeholder w-100 h-100 mb-4 " + (showDefaultImage ? "" : "d-none")}/>
         </div>
         <h4 className="my-4">{news.fakeDetails.fakeTitle}</h4>
         <p className="text-justify px-2">{news.fakeDetails.fakeSummary}</p>
@@ -18,8 +32,9 @@ function CardContent(props) {
   else {
     return (
       <div className='tinder-card p-2'>
-        <div className="img-div">
-          <img src={news.imageUrl} alt={news.title} />
+        <div className="img-div h-100">
+          <img src={news.imageUrl} alt={news.title} className={(showDefaultImage ? "d-none" : "")} onError={imageFirstFailHandler}/>
+          <div className={"placeholder w-100 h-100 mb-4 " + (showDefaultImage ? "" : "d-none")}/>
         </div>
         <h4 className="my-4">{news.title}</h4>
         <p className="text-justify px-2">{news.textSummary}</p>
--- frontend/src/components/SmallCard.js
+++ frontend/src/components/SmallCard.js
@@ -1,15 +1,29 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+
 
 function SmallCard(props) {
 
   const { news } = props;
+  const [showDefaultImage, setShowDefaultImage] = useState(false)
+
+  const imageFirstFailHandler = (e) => {
+    e.target.src = e.target.src.replace(/(^https?:\/\/)?(www.)?arquivo\.pt\/noFrame\/replay\/\d+\//g, "")
+    e.target.onError = imageSecondFailHandler;
+  }
+
+  const imageSecondFailHandler = (e) => {
+    e.target.onError = null;
+    setShowDefaultImage(true)
+  }
 
   if (news.isFake) {
     return (
       <div className="card my-3 red-shadow">
         <div className="d-flex">
           <div className="img-div-sm">
-            <img src={news.fakeDetails.fakeImageUrl} />
+            <img src={news.fakeDetails.fakeImageUrl} alt={news.fakeDetails.fakeTitle} className={(showDefaultImage ? "d-none" : "")}  onError={imageFirstFailHandler}/>
+            <div className={"placeholder h-100 " + (showDefaultImage ? "" : "d-none")}/>
           </div>
           <div className="d-flex align-items-center justify-content-center">
             <p className="m-0 px-3">{news.fakeDetails.fakeTitle}</p>
@@ -23,7 +37,8 @@ function SmallCard(props) {
       <div className="card my-3 green-shadow">
         <div className="d-flex">
           <div className="img-div-sm">
-          <img src={news.imageUrl} />
+            <img src={news.imageUrl} alt={news.title} className={(showDefaultImage ? "d-none" : "")} onError={imageFirstFailHandler} />
+            <div className={"placeholder h-100 " + (showDefaultImage ? "" : "d-none")}/>
           </div>
           <div className="d-flex align-items-center">
           <p className="m-0 px-3">{news.title}</p>
