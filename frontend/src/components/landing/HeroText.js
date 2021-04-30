3fae774b5de87a9ac39f48a39bd5f03f48d226d9
Erroler <fabio_the_boss@hotmail.com>
2021-04-30 10:20:15 +0100

    Made text of play btn on landing page dynamic

--- frontend/src/components/landing/HeroText.js
+++ frontend/src/components/landing/HeroText.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from 'react-router-dom';
+import GameContext from "../../GameContext";
 
 const HeroText = () => {
+
+    const { getGameState } = useContext(GameContext);
+
+    let btnText = '';
+    if (getGameState() === 'INGAME') {
+        btnText = 'Voltar ao Jogo';
+    }
+    else
+        btnText = 'Novo Jogo';
+
     return (
         <div className="col-xl-7 col-12 w-100 m-0">
             <h1 className="my-3 text-md-left" id="landing-title">
@@ -13,7 +24,7 @@ const HeroText = () => {
                 Todos os dias milhares de notícias falsas espalham-se nas redes sociais como um vírus. Achas que as consegues identificar? Mete à prova o teu conhecimento num jogo divertido!                    </p>
             <div className="d-flex flex-column flex-md-row flex-md-row mt-md-0 mt-3">
                 <Link to={'/jogo'} className="btn btn-primary col-md-5 col-lg mr-3 mt-3 mb-0 mb-md-3 py-3 py-md-2">
-                    Novo Jogo
+                    {btnText}
                 </Link>
                 <Link to={'/como-jogar'} className="btn btn-secondary col-md-5 col-lg my-3 py-3 py-md-2">
                     Como Jogar
