c8dbfc24ffd6cd15174b31cba98c87371913cf8e
RMoura98 <ricardo.sanfinsmoura@gmail.com>
2021-04-30 14:11:02 +0100

    Started implementing scrollable tinder cards (temp solution)

--- frontend/src/components/CardContent.js
+++ frontend/src/components/CardContent.js
@@ -5,12 +5,47 @@ function CardContent(props) {
 
   const { news } = props;
   const [showDefaultImage, setShowDefaultImage] = useState(true)
+  
+  let cardRef = React.createRef()
+
+  let scroll;
+
+  const scrollY = (y_dist) => {
+    cardRef.current.scrollBy({ 
+      top: y_dist,
+      behavior: 'smooth' 
+    });
+  }
+  
+  const startScroll = (e) => {
+    if (!cardRef.current) return;
+    var rect = cardRef.current.getBoundingClientRect();
+    var y = null;
+    if (e.clientY) {
+      y = e.clientY - rect.top;  //y position within the element.
+    } else if (e.touches)  {
+      y = e.touches[0].clientY - rect.top;  
+    }
+
+    let threshold = cardRef.current.offsetHeight * .5;
+    if (y >= threshold) {
+      scrollY(18);
+      scroll = setTimeout(() => startScroll(e),100);
+    } else if ((y <= cardRef.current.offsetHeight - threshold)) {
+      scrollY(-18);
+      scroll = setTimeout(() => startScroll(e),100);
+    }
+  }
+
+  const stopScroll = (e) => {
+    clearTimeout(scroll);
+  }
 
   const imageSuccessHandler = () => setShowDefaultImage(false)
 
   if (news.isFake) {
     return (
-      <div className='tinder-card p-2'>
+      <div ref={cardRef} className='tinder-card' onTouchStart={startScroll} onMouseDown={startScroll} onMouseUp={stopScroll} onTouchEnd={stopScroll}>
         <div className="img-div h-100">
           <img src={news.fake_details.fake_image_url?.replace(/(^https?:\/\/)?(www.)?arquivo\.pt\/noFrame\/replay\/\d+\//g, "")} alt={news.fake_details.fake_title} className={(showDefaultImage ? "d-none" : "")} onLoad={imageSuccessHandler} />
           <div className={"placeholder w-100 h-100 mb-4 " + (showDefaultImage ? "" : "d-none")} />
@@ -22,7 +57,7 @@ function CardContent(props) {
   }
   else {
     return (
-      <div className='tinder-card p-2'>
+      <div  ref={cardRef} className='tinder-card'  onTouchStart={startScroll} onMouseDown={startScroll} onMouseUp={stopScroll} onTouchEnd={stopScroll}>
         <div className="img-div h-100">
           <img src={news.image?.replace(/(^https?:\/\/)?(www.)?arquivo\.pt\/noFrame\/replay\/\d+\//g, "")} alt={news.title} className={(showDefaultImage ? "d-none" : "")} onLoad={imageSuccessHandler} />
           <div className={"placeholder w-100 h-100 mb-4 " + (showDefaultImage ? "" : "d-none")} />
