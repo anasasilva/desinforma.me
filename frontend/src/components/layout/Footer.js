8f97dbb8e0f7f30332c9e629bc33b3f0cc5c784a
RMoura98 <ricardo.sanfinsmoura@gmail.com>
2021-05-01 16:34:22 +0100

    Change footer to LinkedIn profiles

--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -3,12 +3,17 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHeart } from '@fortawesome/free-solid-svg-icons'
 
 const Footer = () => {
+
+    let anaProfile = "https://www.linkedin.com/in/anasasilva/";
+    let fabioProfile = "https://www.linkedin.com/in/fabio-vilela-oliveira/";
+    let ricardoProfile = "https://www.linkedin.com/in/rmoura98/";
+
     return (
-        <nav className="navbar transparent navbar-light text-center d-none d-md-block">
+        <nav id="footer" className="navbar transparent navbar-light text-center">
             <small className="font-weight-light mx-auto mb-0">
-                Em colaboração com o <a href="https://arquivo.pt/" className="d-inline" target="_blank">Arquivo.pt</a>. <br className="d-md-none" /> Made with <FontAwesomeIcon icon={faHeart} color="red" /> by <a href="https://github.com/anasasilva" className="d-inline" target="_blanck">Ana</a>
-                , <a href="https://github.com/Erroler" className="d-inline" target="_blank">Fábio</a>
-                , and <a href="https://github.com/RMoura98" className="d-inline" target="_blank">Ricardo</a>.
+                Em colaboração com o <a href="https://arquivo.pt/" className="d-inline" target="_blank" rel="noreferrer">Arquivo.pt</a>. <br className="d-md-none" /> Made with <FontAwesomeIcon icon={faHeart} color="red" /> by <a href={anaProfile} className="d-inline" target="_blanck">Ana</a>
+                , <a href={fabioProfile} className="d-inline" target="_blank" rel="noreferrer">Fábio</a>
+                , and <a href={ricardoProfile} className="d-inline" target="_blank" rel="noreferrer">Ricardo</a>.
             </small>
         </nav>
     )
