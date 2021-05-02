19e374eb0792b1d56fafe2719808a758f203c513
Erroler <fabio_the_boss@hotmail.com>
2021-05-02 12:24:32 +0100

    Refactor of endgame.

--- frontend/src/components/SmallCard.js
+++ frontend/src/components/SmallCard.js
@@ -4,114 +4,122 @@ import ChangedEntity from './ChangedEntity';
 
 
 function SmallCard(props) {
-    
-  const { news, index} = props;
-  const [showDefaultImage, setShowDefaultImage] = useState(false)
 
-  const imageSuccessHandler = () => setShowDefaultImage(false)
+    const { news, index } = props;
+    const [showDefaultImage, setShowDefaultImage] = useState(false)
 
-  if (news.isFake) {
+    const imageSuccessHandler = () => setShowDefaultImage(false)
 
-    
-    const getIndicesOf = (searchStr, str, caseSensitive) => {
-        if (searchStr.length === 0) return [];
-        var startIndex = 0, index, indices = [];
-        while ((index = str.indexOf(searchStr, startIndex)) > -1) {
-            indices.push(index);
-            startIndex = index + searchStr.length;
+    if (news.isFake) {
+
+
+        const getIndicesOf = (searchStr, str, caseSensitive) => {
+            if (searchStr.length === 0) return [];
+            var startIndex = 0, index, indices = [];
+            while ((index = str.indexOf(searchStr, startIndex)) > -1) {
+                indices.push(index);
+                startIndex = index + searchStr.length;
+            }
+            return indices;
         }
-        return indices;
-    }
 
-    
-    const getFalseSummaryFormated = (fullText) => {
-        let fakeTextCopy = fullText;
-        let positionChanges = [];
-        
-        for (let i = 0; i < news.fake_details.entities_replaced.length; i++) {
-            positionChanges.push(getIndicesOf(news.fake_details.entities_replaced[i].replaced_entity_name, fullText));
-            fakeTextCopy = fakeTextCopy.split(news.fake_details.entities_replaced[i].replaced_entity_name).join('^v^');
-        }   
 
-        let outputHtml = [];
-        let totalLength = 0;
-        fakeTextCopy.split('^v^').forEach(textPart => {
-            outputHtml.push(textPart);
-            totalLength += textPart.length;
-            let entityIndex = null;
-            for (let i = 0; i < positionChanges.length; i++) {
-                if (positionChanges[i].includes(totalLength)) {
-                    entityIndex = i;
-                    break;
-                } 
+        const getFalseSummaryFormated = (fullText) => {
+            let fakeTextCopy = fullText;
+            let positionChanges = [];
+
+            for (let i = 0; i < news.fake_details.entities_replaced.length; i++) {
+                positionChanges.push(getIndicesOf(news.fake_details.entities_replaced[i].replaced_entity_name, fullText));
+                fakeTextCopy = fakeTextCopy.split(news.fake_details.entities_replaced[i].replaced_entity_name).join('^v^');
             }
-            if (entityIndex != null) {
-                totalLength += news.fake_details.entities_replaced[entityIndex].replaced_entity_name.length;
-                outputHtml.push([
-                    news.fake_details.entities_replaced[entityIndex].original_entity_name,
-                    news.fake_details.entities_replaced[entityIndex].replaced_entity_name
-                ]);
-            }
-        });
-        return outputHtml;
+
+            let outputHtml = [];
+            let totalLength = 0;
+            fakeTextCopy.split('^v^').forEach(textPart => {
+                outputHtml.push(textPart);
+                totalLength += textPart.length;
+                let entityIndex = null;
+                for (let i = 0; i < positionChanges.length; i++) {
+                    if (positionChanges[i].includes(totalLength)) {
+                        entityIndex = i;
+                        break;
+                    }
+                }
+                if (entityIndex != null) {
+                    totalLength += news.fake_details.entities_replaced[entityIndex].replaced_entity_name.length;
+                    outputHtml.push([
+                        news.fake_details.entities_replaced[entityIndex].original_entity_name,
+                        news.fake_details.entities_replaced[entityIndex].replaced_entity_name
+                    ]);
+                }
+            });
+            return outputHtml;
+        }
+
+
+        return (
+            <div className="card my-3 p-0 nice-shadow hover-toggle " id={"heading_" + news.id}>
+
+                <div className="hover-hide ready d-none d-lg-flex" data-type="fake" />
+                <div className="d-flex" data-toggle="collapse" href={"#collapse_" + news.id} role="button" aria-expanded="false" aria-controls={"collapse_" + news.id}>
+                    <div className="img-div-sm d-none d-md-flex align-items-center">
+                        <img src={news.fake_details.fake_image_url?.replace(/(^https?:\/\/)?(www.)?arquivo\.pt\/noFrame\/replay\/\d+\//g, "")} alt="" className={(showDefaultImage ? "d-none" : "") + "hover-hide"} onLoad={imageSuccessHandler} />
+                        <img src={news.image?.replace(/(^https?:\/\/)?(www.)?arquivo\.pt\/noFrame\/replay\/\d+\//g, "")} alt="" className={"hover-show"} />
+                        <div className={"placeholder h-100 " + (showDefaultImage ? "" : "d-none")} />
+                    </div>
+                    <div className="d-flex flex-column align-items-center justify-content-center w-100 py-2">
+                        <div class="px-3 w-100 d-flex align-items-baseline justify-content-between mb-1">
+                            <span class="badge badge-danger badge-danger-fake-news font-weight-normal">Notícia Falsa</span>&nbsp;
+                            <span class="small text-muted">Adaptado: <a class="d-inline" href={news.url} target="_blank">Publico.pt</a></span>
+                        </div>
+                        <p className="m-0 px-3 pb-0 serif-font font-weight-600 text-justify w-100">
+                            {
+                                getFalseSummaryFormated(news.fake_details.fake_title).map((e) => {
+                                    if (typeof e == "string") return (e)
+                                    else return (<ChangedEntity original={e[0]} created={e[1]} />)
+                                })
+                            }
+                        </p>
+                    </div>
+                </div>
+
+                <div id={"collapse_" + news.id} className={(index === 0 ? "show " : "") + "collapse border-top"} aria-labelledby={"heading_" + news.id} data-parent="#accordion" style={{ backgroundColor: '#fafcff99' }}>
+                    <div className="card-body text-justify">
+                        {getFalseSummaryFormated(news.fake_details.fake_summary).map((e) => {
+                            if (typeof e == "string") return (e)
+                            else return (<ChangedEntity original={e[0]} created={e[1]} />)
+                        })}
+                    </div>
+                </div>
+            </div>
+        )
     }
+    else {
+        return (
 
+            <div className="card my-3 p-0 nice-shadow " id={"heading_" + news.id}>
 
-    return (
-        <div className="card my-3 p-0 nice-shadow serif-font hover-toggle " id={"heading_" + news.id}>
-            
-            <div className="hover-hide ready stamp sm-stamp d-none d-lg-flex" data-type="fake" />
-            <div className="d-flex red-gradiant-overlay" data-toggle="collapse" href={"#collapse_" + news.id} role="button" aria-expanded="false" aria-controls={"collapse_" + news.id}>
-                <div className="img-div-sm d-none d-md-flex">
-                    <img src={news.fake_details.fake_image_url?.replace(/(^https?:\/\/)?(www.)?arquivo\.pt\/noFrame\/replay\/\d+\//g, "")} alt={news.fake_details.fake_title} className={(showDefaultImage ? "d-none" : "") + "hover-hide" } onLoad={imageSuccessHandler} />
-                    <img src={news.image?.replace(/(^https?:\/\/)?(www.)?arquivo\.pt\/noFrame\/replay\/\d+\//g, "")} alt={news.title}  className={"hover-show"} />
-                    <div className={"placeholder h-100 " + (showDefaultImage ? "" : "d-none")} />
+                <div className="d-flex" data-toggle="collapse" href={"#collapse_" + news.id} role="button" aria-expanded="false" aria-controls={"collapse_" + news.id}>
+                    <div className="img-div-sm d-none d-md-flex align-items-center">
+                        <img src={news.image?.replace(/(^https?:\/\/)?(www.)?arquivo\.pt\/noFrame\/replay\/\d+\//g, "")} alt="" className={(showDefaultImage ? "d-none" : "")} onLoad={imageSuccessHandler} />
+                        <div className={"placeholder h-100 " + (showDefaultImage ? "" : "d-none")} />
+                    </div>
+                    <div className="d-flex flex-column align-items-center justify-content-center w-100 py-2">
+                        <div class="px-3 w-100 d-flex align-items-baseline justify-content-between mb-1">
+                            <span class="badge badge-success badge-success-true-news font-weight-normal">Notícia Verdadeira</span>&nbsp;
+                            <span class="small text-muted">Fonte: <a class="d-inline" href={news.url} target="_blank">Publico.pt</a></span>
+                        </div>
+                        <p className="m-0 px-3 pb-0 serif-font font-weight-600 text-justify w-100">{news.title}</p>
+                    </div>
                 </div>
-                <div className="d-flex align-items-center justify-content-center">
-                    <p className="m-0 p-3 py-md-0 serif-font font-weight-600 text-justify">
-                        {
-                            getFalseSummaryFormated(news.fake_details.fake_title).map((e) => {
-                                if (typeof e == "string") return (e)
-                                else return (<ChangedEntity original={e[0]} created={e[1]}/>)
-                            })
-                        }
-                    </p>
+
+                <div id={"collapse_" + news.id} className={(index === 0 ? "show " : "") + "collapse border-top"} aria-labelledby={"heading_" + news.id} data-parent="#accordion" style={{ backgroundColor: '#fafcff99' }}>
+                    <div className="card-body text-justify">{news.summary}</div>
                 </div>
             </div>
 
-            <div id={"collapse_" + news.id} className={(index === 0 ? "show " : "") + "collapse border-top"} aria-labelledby={"heading_" + news.id} data-parent="#accordion" style={{backgroundColor: '#fafcff99'}}>
-                <div className="card-body">
-                    {getFalseSummaryFormated(news.fake_details.fake_summary).map((e) => {
-                        if (typeof e == "string") return (e)
-                        else return (<ChangedEntity original={e[0]} created={e[1]}/>)
-                    })}
-                </div>
-            </div>
-        </div>
-    )
-  }
-  else {
-    return (
-        
-        <div className="card my-3 p-0 nice-shadow serif-font " id={"heading_" + news.id}>
-            
-            <div className="d-flex green-gradiant-overlay" data-toggle="collapse" href={"#collapse_" + news.id} role="button" aria-expanded="false" aria-controls={"collapse_" + news.id}>
-                <div className="img-div-sm d-none d-md-flex">
-                    <img src={news.image?.replace(/(^https?:\/\/)?(www.)?arquivo\.pt\/noFrame\/replay\/\d+\//g, "")} alt={news.title} className={(showDefaultImage ? "d-none" : "")} onLoad={imageSuccessHandler} />
-                    <div className={"placeholder h-100 " + (showDefaultImage ? "" : "d-none")} />
-                </div>
-                <div className="d-flex align-items-center justify-content-center">
-                    <p className="m-0 p-3 serif-font font-weight-600 text-justify">{news.title}</p>
-                </div>
-            </div>
-
-            <div id={"collapse_" + news.id} className={(index === 0 ? "show " : "") + "collapse border-top"} aria-labelledby={"heading_" + news.id} data-parent="#accordion" style={{backgroundColor: '#fafcff99'}}>
-                <div className="card-body">{news.summary}</div>
-            </div>
-        </div>
-      
-    )
-  }
+        )
+    }
 }
 
 export default SmallCard;
--- frontend/src/pages/EndGame.js
+++ frontend/src/pages/EndGame.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect  } from "react";
+import React, { useContext, useEffect } from "react";
 import { Redirect } from "react-router";
 import GameContext from "../GameContext";
 import SmallCard from "../components/SmallCard"
@@ -15,7 +15,7 @@ const EndGame = () => {
     const points = answeredNews.length;
     const maxPoints = parseInt(localStorage.getItem('record-points')) || 0;
 
-    const hours   = Math.floor(getGameDuration() / 3600)
+    const hours = Math.floor(getGameDuration() / 3600)
     const minutes = Math.floor((getGameDuration() - (hours * 3600)) / 60);
     const seconds = getGameDuration() - (hours * 3600) - (minutes * 60);
     let stringTime = "";
@@ -32,7 +32,7 @@ const EndGame = () => {
     let refConfetti = React.createRef()
     useEffect(() => {
         if (brokeRecord) {
-            setTimeout(() => refConfetti?.current.confetti(), 800);
+            setTimeout(() => refConfetti?.current?.confetti(), 800);
         }
     });
 
@@ -40,30 +40,41 @@ const EndGame = () => {
         return <Redirect to='/' />
     }
 
-    
-        
-    
+
+
+
     const infoSection = () => {
         return (
-            <div>   
-                <h1 className="text-center  my-4 my-md-5">Fim do Jogo</h1>
+            <div id="end-game-info">
+                <h1 className="text-center  my-4 my-md-5 display-4">Fim do Jogo</h1>
                 <div className="text-center my-4 my-md-5">
-                    <h5 className="mx-auto pb-2"><FaTrophy/> Pontuação</h5>
-                    <h5 className="mx-auto pb-2">{points} certas</h5>
+                    <h5 className="mx-auto pb-2">
+                        <div className="badge badge-dark p-2 px-3 no-select">
+                            <FaTrophy />&nbsp;&nbsp;Pontuação
+                        </div>
+                    </h5>
+                    <h5 className="mx-auto pb-2">{points} pontos</h5>
                 </div>
                 <div className="text-center my-4 my-md-5">
-                    <h5 className="mx-auto pb-2"><FaHourglass/> Tempo Total</h5>
-                    <h5 className="mx-auto pb-2">{ stringTime} </h5>
+                    <h5 className="mx-auto pb-2">
+                        <div className="badge badge-dark p-2 px-3 no-select">
+                            <FaHourglass />&nbsp;&nbsp;Tempo Gasto
+                        </div>
+                    </h5>
+                    <h5 className="mx-auto pb-2">{stringTime} </h5>
                 </div>
                 <div className="text-center my-4 my-md-5">
-                    <h5 className="mx-auto pb-2">Partilha a tua pontuação</h5>
+                    <h5 className="mx-auto pb-2">Partilha a tua pontuação!</h5>
                     <SocialButtons text={
-                        (brokeRecord ? "Bati o meu record! " : "") + 
+                        (brokeRecord ? "Bati o meu record! " : "") +
                         (hours === 0 && minutes > 1 ?
-                            "Consegui acertar " + points + " noticias em " + minutes + " minutos! ":
+                            "Consegui acertar " + points + " noticias em " + minutes + " minutos! " :
                             "Consegui acertar em " + points + " noticias! ") +
-                        "Achas que consegues fazer melhor? " } 
-                    iconSize="45"/>
+                        "Achas que consegues fazer melhor? "}
+                        iconSize="45" />
+                </div>
+                <div className="text-center my-4 my-md-5">
+                    <a className="btn btn-primary py-3 py-md-2 px-5" href="/jogo">Novo jogo</a>
                 </div>
             </div>
         )
@@ -71,13 +82,14 @@ const EndGame = () => {
 
     const newsSection = () => {
         return (
-                <div id="accordion">
+            <div id="accordion">
+                <h2 className="text-center my-4 my-md-5 h1 font-weight-light">Notícias</h2>
                 {answeredNews.map((news, index) => {
                     return (
-                        <SmallCard news={news} index={index}/>
+                        <SmallCard news={news} index={index} key={index} />
                     )
                 })}
-                </div>
+            </div>
         )
     }
 
@@ -85,21 +97,21 @@ const EndGame = () => {
     return (
         <>
             <ReactCanvasConfetti
-            className="no-pointer-events position-fixed w-100 h-100"
-            ref={refConfetti}
-            style={{ zIndex: 1000 }}
-            origin={{ x: 0.5, y: 0.5 }}
-            particleCount={100}
-            startVelocity={60}
-            gravity={1.2}
-            angle={90}
-            spread={360}
+                className="no-pointer-events position-fixed w-100 h-100"
+                ref={refConfetti}
+                style={{ zIndex: 1000 }}
+                origin={{ x: 0.5, y: 0.5 }}
+                particleCount={100}
+                startVelocity={60}
+                gravity={1.2}
+                angle={90}
+                spread={360}
             />
             <div className="row align-items-center mx-auto w-100">
                 <div className="col-md-5 col-12">
                     {infoSection()}
                 </div>
-                
+
                 <div className="col-md-7 col-12">
                     {newsSection()}
                 </div>
